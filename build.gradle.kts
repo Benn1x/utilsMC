056f12128a7bddf8669b0eaf5021732790e8eddb
BuildTools <xaver@emmerling.cc>
2022-03-27 14:01:30 +0200

    Basis Files

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,6 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
 }
---
+++ src/main/kotlin/com/daskju/utilsMC/logger/Logger.kt
@@ -0,0 +1,21 @@
+package com.daskju.utilsMC.logger
+
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+class Logger {
+    private val LOGGER: Logger? = LogManager.getLogger()
+    fun LoggingWarning(msg:String){
+        LOGGER?.log(Level.WARN, msg)
+    }
+    fun LoggingInfo(msg: String){
+        LOGGER?.log(Level.INFO, msg)
+    }
+    fun LoggingError(msg: String){
+        LOGGER?.log(Level.ERROR, msg)
+    }
+    fun LoggingDebug(msg: String){
+        LOGGER?.log(Level.DEBUG, msg)
+    }
+}
